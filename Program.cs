849a2edd13715e18d31d26ff75888d3ab3a40a1c
nome de usuário do GitHub <Email do GitHub>
2024-10-13 22:30:06 -0300

    calculadorasimples em Program.cs

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using csv1;
+
+        Console.WriteLine("Bem vindo a calculadora simples !!\n");   
+        Console.WriteLine("Escolha uma operação!!\n");
+        Console.WriteLine("1. Soma +\n");
+        Console.WriteLine("2. Multiplicação *\n");
+        Console.WriteLine("3. Divisão / \n");
+        Console.WriteLine("4. Subtração -\n");
+
+        int metodo = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("---------------------------------------------------------------------------------------------------");
+
+        Console.WriteLine("\nDigite um número");
+        double num1 = double.Parse(Console.ReadLine()); 
+
+        Console.WriteLine("\nDigite outro número");
+        double num2 = double.Parse(Console.ReadLine());
+
+        double resultado = 0;
+
+        switch(metodo){
+            case 1:
+                resultado = num1 + num2;
+                Console.WriteLine($"Resultado : { num1} + {num2} = {resultado}");
+                break;
+            case 2:
+                resultado = num1 * num2;
+                Console.WriteLine($"Resultado : { num1} * {num2} = {resultado}");
+                break;
+            case 3:
+                resultado = num1 / num2;
+                Console.WriteLine($"Resultado : { num1} / {num2} = {resultado}");
+                break;
+            case 4:
+                resultado = num1 - num2;
+                Console.WriteLine($"Resultado : { num1} - {num2} = {resultado}");
+                break;
+
+                 default:
+            Console.WriteLine("\nPor gentileza escolha uma opção plaúsivel");
+            break;
+        }
+       
+
+        
+
